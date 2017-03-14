356f9d72431b15b62963b74f0f982c049825acb0
Massivedynamic <massivedynamicplay@gmail.com>
2017-03-14 23:33:03 +0900

    컨플릭트 로딩 헤더

--- ShootingHASH/ShootingHASH/Loading.h
+++ ShootingHASH/ShootingHASH/Loading.h
@@ -1,12 +1,4 @@
 #pragma once
-
-<<<<<<< HEAD
-extern const WCHAR* PartialBG;
-extern const WCHAR* Plane;
-extern const WCHAR* PlaneMask;
-extern const WCHAR* EngineGlow;
-extern const WCHAR* EngineGlowMask;
-=======
 //이미지 파일
 extern const WCHAR* PartialBG;
 extern const WCHAR* PartialPlane;
@@ -29,4 +21,3 @@ extern const float PlaneSpeed;
 //projectile speed
 extern const float PlayerProjectileSpeed;
 
->>>>>>> 18f3c5798a8a896a162e9772e46b9c815cb9d0c9
